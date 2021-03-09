0f85564c0e26f1c8e8077431836548377645ea95
leyli <hosseinzadeleyli.job@gmail.com>
2021-03-09 17:58:51 +0330

    Second commit for log4j

--- src/main/java/controller/Log.java
+++ src/main/java/controller/Log.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,11 +10,14 @@ import java.nio.file.StandardOpenOption;
 
 public class Log {
     private Path path = Paths.get("Files/log.txt");
+    private final Logger logger = Logger.getLogger(Log.class);
 
     public void writeLog(String s) {
+        logger.debug("run Log.writeLog method");
         try {
             Files.write(path, s.getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
+            logger.error("something went wrong ! please check if the log.txt is available or not !'\r\n'" + e.getMessage());
             e.printStackTrace();
         }
     }
